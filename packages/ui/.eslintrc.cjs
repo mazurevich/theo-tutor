be7edf2e1817da19756234bacc8b48d61eaceec6
Andrey Mazurevich <amazurevich@adnoc.ae>
2023-04-07 19:25:03 +0300

    added storybook

---
+++ packages/ui/.eslintrc.cjs
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["custom", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["env.mjs", "vite.config.ts", "tailwind.config.js", "postcss.config.js"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
+
+module.exports = config;
